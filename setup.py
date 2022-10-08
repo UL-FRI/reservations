7f7cf1ed7918e8f1d3020984ab7d55e4bef7741f
Gregor Jerše <gregor.jerse@genialis.com>
2022-10-08 10:32:54 +0200

    Drop support for Python 3.9

--- setup.py
+++ setup.py
@@ -68,7 +68,6 @@ setuptools.setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
     ],
     keywords="FRI reservations django",
