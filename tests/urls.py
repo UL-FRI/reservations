8afcd8ae1d1eab2440f1b4c637f66de87911fa90
Gregor Jerše <gregor.jerse@genialis.com>
2022-12-22 13:44:01 +0100

    Use black on tetst folder

--- tests/urls.py
+++ tests/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('reservations.urls')),
-    path('__debug__/', include('debug_toolbar.urls')),
+    path("admin/", admin.site.urls),
+    path("", include("reservations.urls")),
+    path("__debug__/", include("debug_toolbar.urls")),
 ]
